92b775fabb0e87386992138d399dfdc139265ca7
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-04-22 13:36:55 -0700

    Helper struct in Expresission result to track types

--- include/ExpressionResult.h
+++ include/ExpressionResult.h
@@ -1,1 +1,12 @@
-// ExpressionResult.h = Use as a helper func to track the type checks 
+#ifndef EXPRESSION_RESULT_H
+#define EXPRESSION_RESULT_H
+
+#include <string>
+
+// Helper struct for Assignment 3 expression checking
+// Will help with expressions needed to track their result type
+struct ExpressionResult {
+    std::string type;
+};
+
+#endif 
