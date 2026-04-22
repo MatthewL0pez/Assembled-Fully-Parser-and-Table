1f058e6669ddb1382383b4efae62e576a12655cd
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-04-22 12:10:50 -0700

    print symbol table out

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -4,7 +4,7 @@
 SymbolTable::SymbolTable() : nextMemLocation_(10000) {
 }
 
-// Add a new id if it is not already in the symbol table
+// Add a new identifier if it is not already in the symbol table
 bool SymbolTable::add(const std::string& name, const std::string& type) {   
     if (exists(name)) {
         return false; 
@@ -19,4 +19,27 @@ bool SymbolTable::add(const std::string& name, const std::string& type) {
     nextMemLocation_++;
 
     return true;
+}
+
+// Check if an identifier is already declared in the Table 
+bool SymbolTable::exists(const std::string& name) const {
+    for (const auto& entry : entries_) {
+        if (entry.identifier == name) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Print the symbol table out
+void SymbolTable::print(std::ofstream& out) const {
+    out << "\nSymbol Table\n";
+    out << "Identifier\tMemoryLocation\tType\n";
+
+    for (const auto& entry : entries_) {
+        out << entry.identifier << '\t'
+            << entry.memLocation << '\t'
+            << entry.type << '\n';
+    }
 }
