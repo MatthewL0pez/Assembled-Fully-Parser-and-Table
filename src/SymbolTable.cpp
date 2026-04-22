7677a7aaa508a4461a833fc4772b0909e6d0bb3c
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-04-22 11:56:19 -0700

    SymbolTable initialized memory location from examples

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -1,1 +1,5 @@
-// SymbolTable cpp 
+#include "SymbolTable.h"
+
+// begin the memory location at 10000 from assignment instructions and EXAMPLE
+SymbolTable::SymbolTable() : nextMemLocation_(10000) {
+}
