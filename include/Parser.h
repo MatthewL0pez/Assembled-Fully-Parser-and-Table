a8d58559a4b762aa3523000357660c5aad8cbbd4
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-04-22 13:43:12 -0700

    Added parser funcs from Tables

--- include/Parser.h
+++ include/Parser.h
@@ -3,13 +3,17 @@
 
 #include "Lexer.h"
 #include "Tokens.h"
+#include "SymbolTable.h"
+#include "InstructionTable.h"
+#include "ExpressionResult.h"
 #include <fstream> 
 #include <string> 
 
 class Parser {
-    public:
+public:
     Parser(Lexer& lexer, std::ofstream& outFile, bool printRules = true);
-    // PERSON 1 + the general parser struct
+    
+    // grammer 
     void Rat26S();
     void OptFunctionDefinitions();
     void FunctionDefinitions();
@@ -36,7 +40,7 @@ class Parser {
     void Scan();
     void While();
     
-    // PERSON 3 rules
+    // Expreesions, conditions
     void Condition();
     void Relop();
     void Expression();
@@ -45,17 +49,28 @@ class Parser {
     void TermPrime();
     void Factor();
     void Primary();
+
+    // FOR ASSIGNMENT 3 TABLES
+    void printSymbolTable(); 
+    void printInstructionTable();
     
-    private: 
+private: 
     Lexer& lexer_; //connects lexer 
     Token currentToken_; //stores current token being analyzed 
     std::ofstream& out_; // this gives the output file 
     bool printRules_; //allows for the printing of the grammar rules to be printed or not 
+
+    // FOR ASSIGNMENT 3 TABLES
+    SymbolTable symbolTable_;
+    InstructionTable instructionTable_;
+    std::string currentDeclarationType_; 
+
     void advance(); //moves to next token 
     void match(int expectedCategory, const std::string& expectedLexeme = ""); //checks if token matches 
     void printToken(); //prints token + lexeme 
     void printProduction(const std::string& rule); //prints grammar rule 
     void error(const std::string& message); //hanndles sytax errors
+
     bool isQualifier() const; //checks if token is int bool or real 
     bool isStatementStart() const; // check to begin statement
     bool isRelopToken() const; // check tokecn to RELOOP through statemtn
