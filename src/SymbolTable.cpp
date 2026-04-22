7e8455242eef8fe9ee716451880240c1f67566a4
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-04-22 12:03:51 -0700

    SymbolTable Add identifier to stack

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -3,3 +3,20 @@
 // begin the memory location at 10000 from assignment instructions and EXAMPLE
 SymbolTable::SymbolTable() : nextMemLocation_(10000) {
 }
+
+// Add a new id if it is not already in the symbol table
+bool SymbolTable::add(const std::string& name, const std::string& type) {   
+    if (exists(name)) {
+        return false; 
+    }
+
+    SymbolTableEntry newEntry;
+    newEntry.identifier = name;
+    newEntry.memLocation = nextMemLocation_;
+    newEntry.type = type;
+
+    entries_.push_back(newEntry);
+    nextMemLocation_++;
+
+    return true;
+}
