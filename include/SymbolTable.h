df4c31f3bf28eb945408332b292ce3b78c5e7028
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-04-22 11:37:40 -0700

    Symboltable Entry class

--- include/SymbolTable.h
+++ include/SymbolTable.h
@@ -1,1 +1,13 @@
-// Symbol table header 
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+struct SymbolTableEntry {   // One row in the symbol table
+    std::string identifier;
+    int memoryLocation;
+    std::string type;
+};
+
